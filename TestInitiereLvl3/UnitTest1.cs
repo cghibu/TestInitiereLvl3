29cb201b31e5e66b080c18b83a25709ebb2ab7c2
Calin Ghibu <cghibu@gmail.com>
2015-11-18 15:54:55 +0200

    first go

---
+++ TestInitiereLvl3/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestInitiereLvl3
+{
+
+    public struct BaseConvertStructure
+    {
+        public uint baseToConvertTo;
+        public uint numberToConvert;
+        byte[] numberInBaseAsByteString;
+
+        public void AllocateStorage()
+        {
+            uint powerOfBase = (uint)Math.Truncate(Math.Log(numberToConvert, baseToConvertTo));
+            numberInBaseAsByteString = new byte[powerOfBase + 1];         
+        }
+
+        public BaseConvertStructure(uint inputBase, uint inputNumber)
+        {
+            baseToConvertTo = inputBase;
+            numberToConvert = inputNumber;
+            numberInBaseAsByteString = null;
+        }
+
+        public void ConvertInputToBase()
+        {
+
+            AllocateStorage();
+            uint mod = numberToConvert / baseToConvertTo;
+            uint reminder = numberToConvert % baseToConvertTo;
+            int indexToInsertTo = 0;
+            if (mod > baseToConvertTo)
+            {
+                numberInBaseAsByteString[indexToInsertTo] = (byte)reminder;
+                indexToInsertTo = indexToInsertTo + 1;
+                ConvertInputToBase()
+            }
+            
+
+
+
+
+        }
+        
+    }
+
+    [TestClass]
+    public class TestBaseConvertor
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
